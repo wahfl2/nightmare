1aca4ed74f5823ab6e1eac43ade6332245565a8e
wahfl2 <59855656+wahfl2@users.noreply.github.com>
2023-10-04 22:26:55 -0500

    Improve screen scroll

--- camera2.gd
+++ camera2.gd
@@ -1,19 +1,37 @@
 extends Camera2D
 
 const SCREEN_SIZE = Vector2(1920, 1080)
+const CAM_MOVE_DELTA = 0.001
+const CAM_POS_EPSILON = 7.5
+const TRANSITION_FREEZE_TIME = 0.2
+
+var prev_target_pos = Vector2(0, 0)
+var time_since_transition = TRANSITION_FREEZE_TIME
+
+func is_within_epsilon(left: Vector2, right: Vector2, epsilon: float):
+	return right.x >= (left.x - epsilon) && right.x <= (left.x + epsilon) && \
+		right.y >= (left.y - epsilon) && right.y <= (left.y + epsilon)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	process_mode = Node.PROCESS_MODE_ALWAYS
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var player_pos = $"../World/Player".position
-	var target_pos = round(player_pos / SCREEN_SIZE) * SCREEN_SIZE
+	time_since_transition += delta
 	
-	if position != target_pos:
+	var player_pos = $"../World/Player".position
+	var target_pos: Vector2 = round(player_pos / SCREEN_SIZE) * SCREEN_SIZE
+	
+	if !target_pos.is_equal_approx(prev_target_pos):
+		prev_target_pos = target_pos
+		time_since_transition = 0.0
+	
+	for i in delta / CAM_MOVE_DELTA:
+		position += (target_pos - position) / 200
+	
+	if time_since_transition < TRANSITION_FREEZE_TIME:
 		$"../World".process_mode = Node.PROCESS_MODE_DISABLED
-		position += (target_pos - position) / 5
 	else:
 		$"../World".process_mode = Node.PROCESS_MODE_ALWAYS
